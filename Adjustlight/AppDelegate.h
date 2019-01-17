b4ccbc44f78ba395b64bc583409797bcfe971bca
ouyangdan <ouyangdan@ewang.com>
2019-01-17 14:39:13 +0800

    Initial Commit

---
+++ Adjustlight/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Adjustlight
+//
+//  Created by henry on 2019/1/17.
+//  Copyright © 2019 henry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Adjustlight/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Adjustlight
+//
+//  Created by henry on 2019/1/17.
+//  Copyright © 2019 henry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
